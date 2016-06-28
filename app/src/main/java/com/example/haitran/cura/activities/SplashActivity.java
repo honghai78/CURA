1fc7a1c02b3bd7d92dd893904e639968d801dc45
Hai Tran <hai.tran@infonam.com>
2016-06-28 11:22:58 +0700

    CURA project training

---
+++ app/src/main/java/com/example/haitran/cura/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.haitran.cura.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.haitran.cura.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
