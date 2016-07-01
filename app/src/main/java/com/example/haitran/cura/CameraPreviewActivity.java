649a1d6a4a5437013276b7872ae56b2f7bd9189f
Hai Tran <hai.tran@infonam.com>
2016-07-01 09:10:55 +0700

    update fragment

---
+++ app/src/main/java/com/example/haitran/cura/CameraPreviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.haitran.cura;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraPreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_camera_preview);
+    }
+}
--- app/src/main/java/com/example/haitran/cura/others/PhotoHandler.java
+++ app/src/main/java/com/example/haitran/cura/others/PhotoHandler.java
@@ -1,6 +1,8 @@
 package com.example.haitran.cura.others;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Environment;
@@ -25,34 +27,35 @@ public class PhotoHandler implements PictureCallback {
     @Override
     public void onPictureTaken(byte[] data, Camera camera) {
 
-        File pictureFileDir = getDir();
-
-        if (!pictureFileDir.exists() && !pictureFileDir.mkdirs()) {
-
-            Toast.makeText(context, "Can't create directory to save image.",
-                    Toast.LENGTH_LONG).show();
-            return;
-
-        }
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyyyddMMyyyyHHmmss");
-        String date = dateFormat.format(new Date());
-        String photoFile = "Picture_" + date + ".jpg";
-
-        String filename = pictureFileDir.getPath() + File.separator + photoFile;
-
-        File pictureFile = new File(filename);
-
-        try {
-            FileOutputStream fos = new FileOutputStream(pictureFile);
-            fos.write(data);
-            fos.close();
-            Toast.makeText(context, "New Image saved:" + photoFile,
-                    Toast.LENGTH_LONG).show();
-        } catch (Exception error) {
-            Toast.makeText(context, "Image could not be saved.",
-                    Toast.LENGTH_LONG).show();
-        }
+        Bitmap bitmap = BitmapFactory.decodeByteArray(data , 0, data.length);
+//        File pictureFileDir = getDir();
+//
+//        if (!pictureFileDir.exists() && !pictureFileDir.mkdirs()) {
+//
+//            Toast.makeText(context, "Can't create directory to save image.",
+//                    Toast.LENGTH_LONG).show();
+//            return;
+//
+//        }
+//
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMyyyyddMMyyyyHHmmss");
+//        String date = dateFormat.format(new Date());
+//        String photoFile = "Picture_" + date + ".jpg";
+//
+//        String filename = pictureFileDir.getPath() + File.separator + photoFile;
+//
+//        File pictureFile = new File(filename);
+//
+//        try {
+//            FileOutputStream fos = new FileOutputStream(pictureFile);
+//            fos.write(data);
+//            fos.close();
+//            Toast.makeText(context, "New Image saved:" + photoFile,
+//                    Toast.LENGTH_LONG).show();
+//        } catch (Exception error) {
+//            Toast.makeText(context, "Image could not be saved.",
+//                    Toast.LENGTH_LONG).show();
+//        }
     }
 
     private File getDir() {
